8171ad8721daa8f463b78a84bbb94b299898a9ed
Jennifer Currie <renestl@gmail.com>
2017-01-06 20:22:15 -0600

    testing ip address

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', function(req, res) {
 
 app.get('/api/whoami', function(req, res) {
 
-	var address = req.headers['host'];
+	var address = req.ip;
 	var language = req.headers['accept-language'].split(',')[0];
 	var opSys =  req.headers['user-agent'].match(/\([^\(\)\n]*\)/)[0].split("(")[1].split(")")[0];
 
