daa2b61824ecabb885c1ed36de0396b226cf9e6a
Jennifer Currie <renestl@gmail.com>
2017-01-06 20:43:36 -0600

    updated ip address function

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.get('/', function(req, res) {
 
 app.get('/api/whoami', function(req, res) {
 
-	var address = req.headers['host'];
+	var address = req.ip;
+		if(address.substr(0,7) === "::ffff:"){
+			address = address.substr(7);
+		}
 	var language = req.headers['accept-language'].split(',')[0];
 	var opSys =  req.headers['user-agent'].match(/\([^\(\)\n]*\)/)[0].split("(")[1].split(")")[0];
 
@@ -23,8 +26,8 @@ app.get('*', function(req, res) {
 	res.render('home');
 });
 
-var port = process.env.PORT || 3000;
-  app.listen(port);
+// var port = process.env.PORT || 3000;
+//   app.listen(port);
 
 // app.listen(3000, '127.0.0.1', function() {
 // 	console.log('Server has started!');
