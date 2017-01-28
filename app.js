64ff2058c01f56fffcd38146752ecc38ffc3d28b
Jennifer Currie <renestl@gmail.com>
2017-01-27 19:25:49 -0600

    testing ip address

--- app.js
+++ app.js
@@ -10,12 +10,10 @@ app.get('/', function(req, res) {
 
 app.get('/api/whoami', function(req, res) {
 
-	var address = req.ip ;
-		if(address.substr(0,7) === "::ffff:"){
-			address = address.substr(7);
-		}
-
-	// var address = req.ip || req.headers['x-forwarded-for'];
+	var address = req.headers['x-forwarded-for'] ;
+// 		if(address.substr(0,7) === "::ffff:"){
+// 			address = address.substr(7);
+// 		}
 
 	var language = req.headers['accept-language'].split(',')[0];
 	var opSys =  req.headers['user-agent'].match(/\([^\(\)\n]*\)/)[0].split("(")[1].split(")")[0];
